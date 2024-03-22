27b3bc6a4ec5ec1c2ecbb4f282b7a84fd6b4d04a
顾霖轩 <2495802072@qq.com>
2024-03-22 15:09:44 +0800

    完善部分代码不合规命名
    编写世界创建过程，持续中

---
+++ World/script/Core/area_block.gd
@@ -0,0 +1,5 @@
+## 区块类
+class_name AreaBlock extends Resource
+
+@export var id:Vector2
+@export var array:Array[Vector3]
---
+++ World/script/world_data.gd
@@ -0,0 +1,5 @@
+class_name WorldData extends Resource
+
+@export var world_name:String
+@export var difficult : = WorldManager.DIFFICULTIES.EASY
+@export var block:Array[AreaBlock]
